62aca69196801465a2d25fb690273dd76a7b3efe
Rico Tandrio <119276763+ricotandrio@users.noreply.github.com>
2024-11-14 13:47:40 +0700

    fix: streamline search functionality and improve query handling

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,19 +14,23 @@ class SearchController extends Controller
             'asc' => ['nullable', 'boolean']
         ]);
 
-        $validated_request['query'] = $validated_request['query'] ?? '';
-
-        $products = Product::where('name', 'like', '%' . $validated_request['query'] . '%')->paginate(10)->withQueryString() ?? [];
+        $validated_request['query'] = $validated_request['query'] ?: '';
 
         if(isset($validated_request['order_by'])) {
             $ascending = $validated_request['asc'] ?? true;
-
+            
             $products = Product::where('name', 'like', '%' . $validated_request['query'] . '%')
-                ->orderBy($validated_request['order_by'], $ascending ? 'asc' : 'desc')
-                ->paginate(10)
-                ->withQueryString();
+            ->orderBy($validated_request['order_by'], $ascending ? 'asc' : 'desc')
+            ->paginate(10)
+            ->withQueryString();
+
+            return view('search', [
+                'products' => $products
+            ]);
         }
 
+        $products = Product::where('name', 'like', '%' . $validated_request['query'] . '%')->paginate(10)->withQueryString() ?? [];
+            
         return view('search', [
             'products' => $products
         ]);
