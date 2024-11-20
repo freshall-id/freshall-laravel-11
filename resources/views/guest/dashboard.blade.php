4927c8c893ad8343c675f0e515caf370ea96cee8
Rico Tandrio <119276763+ricotandrio@users.noreply.github.com>
2024-11-20 14:39:56 +0700

    Fix: update cart items check to use count for better accuracy

--- resources/views/guest/dashboard.blade.php
+++ resources/views/guest/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @auth
-        @if (Auth::user()->cart->cartItems)
+        @if (Auth::user()->cart->cartItems->count() > 0)
             <a href="{{ route('cart.page') }}" class="position-relative">
                 <div class="position-fixed d-none d-sm-block text-light m-2 rounded row z-max p-2 shadow" style="background-color: var(--accent-yellow); right: 0.5rem; bottom: 0.5rem;">
                     <i class="fa-solid fa-bag-shopping d-flex align-items-center justify-content-center p-0 m-0" style="font-size: 2rem; width: 3rem; height: 3rem;"></i>
