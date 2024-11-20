2a15187b3535c6fd5351793020928fb26ea203b5
Rico Tandrio <119276763+ricotandrio@users.noreply.github.com>
2024-11-20 14:22:20 +0700

    Refactor: remove CartStatus component and integrate cart status directly in the dashboard view

--- resources/views/guest/dashboard.blade.php
+++ resources/views/guest/dashboard.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <x-cart-status />
+    @auth
+        @if (Auth::user()->cart->cartItems)
+            <a href="{{ route('cart.page') }}" class="position-relative">
+                <div class="position-fixed d-none d-sm-block text-light m-2 rounded row z-max p-2 shadow" style="background-color: var(--accent-yellow); right: 0.5rem; bottom: 0.5rem;">
+                    <i class="fa-solid fa-bag-shopping d-flex align-items-center justify-content-center p-0 m-0" style="font-size: 2rem; width: 3rem; height: 3rem;"></i>
+                    <span class="position-absolute bg-danger p-0" style="width: 0.8rem; aspect-ratio: 1/1; border-radius: 100%; right: 0.6rem; top: 1rem;">
+                    </span>
+                </div>
+            </a>
+        @endif
+    @endauth
+
     <section>
         <section class="py-sm-2 p-0 m-0">
             <div id="promotionCarousel" class="carousel slide" data-bs-ride="carousel">
