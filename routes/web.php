890901dfab3e3ff67ff5e2d68fac1be881db8c76
jonathancalvin <jonathancalvin16@gmail.com>
2024-11-22 13:50:55 +0700

    fix: update controller alias and clean up route definitions

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\Admin\DashboardController as DashboardAdminController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\VoucherController;
 use Illuminate\Support\Facades\Route;
@@ -53,42 +53,38 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    
+
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout.page');
-    
-    Route::put('update-transaction-header/{id}', [TransactionController::class, 'updateTransactionHeader'])->name('update-transaction-header.action');
-    Route::delete('delete-transaction-header/{transactionHeader}', [TransactionController::class, 'deleteTransactionHeader'])->name('delete-transaction-header.action');
-    
-    
+
     Route::get('/checkout', [CartController::class, 'viewCheckoutPage'])->name('checkout.page');
-    
+
     Route::put('/update-shipping-provider', [CartController::class, 'updateShippingProvider'])->name('update-shipping-provider.action');
-    
+
     Route::prefix('voucher')->group(function () {
         Route::get('/use/{voucher}', [VoucherController::class, 'getVoucher'])->name('use-voucher.page');
         Route::post('/use', [VoucherController::class, 'useVoucher'])->name('use-voucher.action');
     });
-    
+
     Route::prefix('/cart')->group(function () {
         Route::get('/', [CartController::class, 'viewCartPage'])->name('cart.page');
-        
+
         Route::post('/checkout', [CartController::class, 'checkout'])->name('checkout.action');
         Route::post('/add-to-cart/{cart}/{product}', [CartController::class, 'addToCart'])->name('add-to-cart.action');
-        
+
         Route::put('/increment/{cart_item}', [CartController::class, 'incrementCartItem'])->name('update-cart-item.increment.action');
         Route::put('/decrement/{cart_item}', [CartController::class, 'decrementCartItem'])->name('update-cart-item.decrement.action');
         Route::put('/update-cart-item/{cart_item}/{status}', [CartController::class, 'updateCartItem'])->name('update-cart-item.action');
-        
+
         Route::delete('/delete-cart-item/{cart_item}', [CartController::class, 'deleteCartItem'])->name('delete-cart-item.action');
     });
 });
 
 Route::middleware('admin')->prefix('/admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'viewAdminDashboardPage'])->name('admin-dashboard.page');
+    Route::get('/dashboard', [AdminDashboardController::class, 'viewDashboardPage'])->name('admin-dashboard.page');
     Route::put('/update-transaction-header/{id}', [TransactionController::class, 'updateTransactionHeader'])->name('update-transaction-header.action');
     Route::delete('/delete-transaction-header/{transactionHeader}', [TransactionController::class, 'deleteTransactionHeader'])->name('delete-transaction-header.action');
-    Route::post('/product',[ProductController::class,'storeCreatedProduct'])->name('create.product.action');
-    Route::get('/product/create',[ProductController::class,'createProduct'])->name('create.product.page');
+    Route::post('/product', [ProductController::class, 'storeCreatedProduct'])->name('create.product.action');
+    Route::get('/product/create', [ProductController::class, 'createProduct'])->name('create.product.page');
 });
 
 Route::get('/', [DashboardController::class, 'viewDashboardPage'])->name('dashboard.page');
@@ -96,9 +92,9 @@ Route::get('/search/{order_by?}/{asc?}', [SearchController::class, 'viewSearchPa
 
 Route::get('/product/{product}', [ProductController::class, 'viewProductDetailPage'])->name('product-detail.page');
 
-Route::view('/TermsAndConditions','guest.terms-and-conditions')->name('termsandconditions.page');
-Route::view('/PrivacyPolicy','guest.privacy-policy')->name('privacypolicy.page');
-Route::view('/About','guest.about')->name('about.page');
+Route::view('/TermsAndConditions', 'guest.terms-and-conditions')->name('termsandconditions.page');
+Route::view('/PrivacyPolicy', 'guest.privacy-policy')->name('privacypolicy.page');
+Route::view('/About', 'guest.about')->name('about.page');
 
 Route::prefix('/product-category')->group(function () {
     Route::get('/label/{label}', [ProductCategoryController::class, 'viewProductCategoryByLabelPage'])->name('product-category-by-label.page');
